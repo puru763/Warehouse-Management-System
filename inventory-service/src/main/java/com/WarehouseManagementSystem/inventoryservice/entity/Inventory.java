925f193e800c35dca6af7b5ed09d66efdf13ae56
Purvesh Krishnani <purvesh152001@gmail.com>
2024-05-23 20:10:43 +0530

    ..

--- inventory-service/src/main/java/com/WarehouseManagementSystem/inventoryservice/entity/Inventory.java
+++ inventory-service/src/main/java/com/WarehouseManagementSystem/inventoryservice/entity/Inventory.java
@@ -1,2 +1,22 @@
-package com.WarehouseManagementSystem.inventoryservice.entity;public class Inventory {
+package com.WarehouseManagementSystem.inventoryservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "inventory")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Integer stock;
 }
--- inventory-service/src/main/java/com/WarehouseManagementSystem/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/WarehouseManagementSystem/inventoryservice/repository/InventoryRepository.java
@@ -1,2 +1,10 @@
-package com.WarehouseManagementSystem.inventoryservice.repository;public interface InventoryRepository {
+package com.WarehouseManagementSystem.inventoryservice.repository;
+
+import com.WarehouseManagementSystem.inventoryservice.entity.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    Optional<Inventory> findBySkuCode(String skuCode);
 }
